c290b34c95fde5253c0c213c7edee38d00a6cd19
Andrey Tanchak <tanchak12@gmail.com>
2020-02-28 08:49:58 +0200

    added methods sum and div

---
+++ src/main/java/testim_gilki.java
@@ -0,0 +1,16 @@
+public class testim_gilki {
+    public static void main(String[] args){
+        int c= 10;
+        int d=17;
+        System.out.println(sum(c,d));
+        System.out.println(div(c,d));
+
+    }
+    public static int sum(int a,int b){
+        return a+b;
+    }
+    public static int div(int a,int b){
+        return a-b;
+
+    }
+}
